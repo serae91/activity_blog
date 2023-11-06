7b516d3bdbf024fb173eb3b09b2144bbf0003d1a
Seraphin Aeschbach <seae@if.de>
2023-11-06 21:41:42 +0100

    test-db-data-works

--- backend/src/main/resources/db/migration/V2.0.0__tables.sql
+++ backend/src/main/resources/db/migration/V2.0.0__tables.sql
@@ -1,31 +1,36 @@
-CREATE TABLE activity
-(
-    activity_id     bigint                      NOT NULL,
-    title           character varying(255)      NOT NULL,
-    description     character varying(255)      DEFAULT '',
-    post_time       timestamp without time zone NOT NULL,
-    CONSTRAINT activity_pkey PRIMARY KEY (activity_id)
-);
-
 CREATE TABLE person
 (
     person_id       bigint                      NOT NULL,
     first_name      character varying(255)      NOT NULL,
     last_name       character varying(255)      NOT NULL,
     birthday        DATE                        NOT NULL,
-    CONSTRAINT person_pkey PRIMARY KEY (person_id)
+    CONSTRAINT person_pkey PRIMARY KEY (person_id),
+    CONSTRAINT person_attributes_unique   UNIQUE (first_name, last_name, birthday)
 );
 
+CREATE TABLE activity
+(
+    activity_id     bigint                      NOT NULL,
+    author_id       bigint                      NOT NULL,
+    title           character varying(255)      NOT NULL,
+    description     character varying(255)      DEFAULT '',
+    post_time       timestamp without time zone NOT NULL,
+    CONSTRAINT activity_pkey PRIMARY KEY (activity_id),
+    CONSTRAINT author_fkey   FOREIGN KEY (author_id)
+        REFERENCES person (person_id)
+);
 CREATE TABLE location
 (
     location_id     bigint                      NOT NULL,
     name            character varying(255)      NOT NULL,
     country         character varying(255)      NOT NULL,
-    city            character varying(255)      DEFAULT '',
-    street          character varying(255)      DEFAULT '',
-    street_number   smallint,
-    postal_code     smallint,
-    CONSTRAINT location_pkey PRIMARY KEY (location_id)
+    city            character varying(255)      NOT NULL,
+    postal_code     integer                     NOT NULL,
+    street          character varying(255)      NOT NULL,
+    street_number   integer                     NOT NULL,
+    CONSTRAINT location_pkey PRIMARY KEY (location_id),
+    CONSTRAINT location_name_unique   UNIQUE (name),
+    CONSTRAINT location_attributes_unique   UNIQUE (country, postal_code, street, street_number)
 );
 
 CREATE TABLE activity_person
---
+++ backend/src/main/resources/db/testdata/V2.0.1__table_entries.sql
@@ -0,0 +1,13 @@
+INSERT INTO person VALUES (nextval('person_sequence'), 'Herbert', 'Schuhbauer', '1978-04-01');
+INSERT INTO person VALUES (nextval('person_sequence'), 'Melanie', 'Gruber', '1993-11-23');
+
+WITH persons AS (
+    SELECT person_id AS id FROM person WHERE first_name='Herbert' AND last_name='Schuhbauer' AND BIRTHDAY='1978-04-01'
+    )
+INSERT INTO activity VALUES (nextval('activity_sequence'), (SELECT id FROM persons),'New Blog Page App', 'Today I wrote a new Blog Page App, it was fun', CURRENT_TIMESTAMP);
+
+INSERT INTO location VALUES (nextval('location_sequence'), 'Fischbrunnen', 'Germany', 'Munich', 80919, 'Marienplatz', 11);
+
+INSERT INTO activity_person VALUES (nextval('activity_person_sequence'), currval('activity_sequence'), currval('person_sequence'));
+
+INSERT INTO activity_location VALUES (nextval('activity_location_sequence'), currval('activity_sequence'), currval('location_sequence'));
