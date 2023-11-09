5a21116c60e7cfd473c9b78f0a1eca074fa7fa4b
Seraphin Aeschbach <seae@if.de>
2023-11-10 00:09:17 +0100

    handle person ids

--- frontend/src/app/core/services/app-config/app-config.service.ts
+++ frontend/src/app/core/services/app-config/app-config.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
 
 export interface AppConfig {
   version: string;
--- frontend/src/app/pages/activity-list/create-activity-modal/create-activity-modal.component.ts
+++ frontend/src/app/pages/activity-list/create-activity-modal/create-activity-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { UntypedFormArray, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { FormArray, FormControl, UntypedFormArray, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { MatOptionSelectionChange } from '@angular/material/core';
+import { LocationDto } from 'src/app/_api/location.dto';
 import { PersonDto } from 'src/app/_api/person.dto';
+import { LocationService } from 'src/app/core/services/location/location.service';
 import { PersonService } from 'src/app/core/services/person/person.service';
 
 @Component({
@@ -11,18 +14,44 @@ import { PersonService } from 'src/app/core/services/person/person.service';
 export class CreateActivityModalComponent {
   formGroup: UntypedFormGroup;
   persons: PersonDto[];
+  locations: LocationDto[];
 
-  constructor(private formBuilder: UntypedFormBuilder, private personService: PersonService) {}
+  get personIds(): FormArray {
+    return this.formGroup.controls['personIds'] as FormArray;
+  }
+
+  get locationIds(): FormArray {
+    return this.formGroup.controls['personIds'] as FormArray;
+  }
+
+  constructor(private formBuilder: UntypedFormBuilder, private personService: PersonService, private locationService: LocationService) {}
 
   ngOnInit(): void {
+    this.loadAllPersons();
+    this.loadAllLocations();
+    this.initFormGroup();
+  }
+
+  loadAllPersons(): void {
     this.personService.getAllPersons().subscribe(persons => this.persons = persons);
+  }
+
+  loadAllLocations(): void {
+    this.locationService.getAllLocations().subscribe(locations => this.locations = locations);
+  }
+
+  initFormGroup(): void {
     this.formGroup = this.formBuilder.group({
-      authorId: new UntypedFormControl(-1),
-      title: new UntypedFormControl(''),
-      description: new UntypedFormControl(''),
-      postTime: new UntypedFormControl(''),
-      personIds: new UntypedFormArray([new UntypedFormControl('Max')]),
-      locationIds: new UntypedFormArray([]),
+      authorId: this.formBuilder.control(-1),
+      title: this.formBuilder.control(''),
+      description: this.formBuilder.control(''),
+      postTime: this.formBuilder.control(''),
+      personIds: this.formBuilder.array([this.formBuilder.control(0)]),
+      locationIds: this.formBuilder.array([]),
     });
+  }
+
+  onPersonSelectionChange(event: MatOptionSelectionChange, index: number): void {
+    this.personIds.controls[index].setValue(event.source.value);
   }
 }
